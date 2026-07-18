b26586e72343994ba41747d3147d66c823cb541d
preamer <1524477126@qq.com>
2026-07-18 18:07:24 +0800

    1. use decorator to register boundary class 2. update boundary

--- src/fv/boundary.py
+++ src/fv/boundary.py
@@ -1,7 +1,24 @@
 from dataclasses import dataclass
 
 
+class BoundaryFactory:
+    _REGISTRY = {}
+
+    @classmethod
+    def register(cls, type_name: str):
+        def decorator(subclass):
+            cls._REGISTRY[type_name] = subclass
+            return subclass
+        return decorator
+
+    @classmethod
+    def create(cls, name: str, id_: str, type_: str):
+        boundary_cls = cls._REGISTRY.get(type_, NotImplementedBoundary)
+        return boundary_cls(name, id_)
+
+
 @dataclass
+@BoundaryFactory.register('fluid')
 class Fluid:
     name: str
     id_: str
@@ -17,6 +34,7 @@ class Fluid:
 
 
 @dataclass
+@BoundaryFactory.register('solid')
 class Solid:
     name: str
     id_: str
@@ -25,6 +43,7 @@ class Solid:
 
 
 @dataclass
+@BoundaryFactory.register('velocity-inlet')
 class VelocityInlet:
     name: str
     id_: str
@@ -50,7 +69,7 @@ class VelocityInlet:
 
     _KE_SPEC = {
         '1': 'Intensity and Length Scale',
-        '2': 'Intensith and Viscosity Ratio',
+        '2': 'Intensity and Viscosity Ratio',
         '3': 'Intensity and Hydraulic Diameter',
     }
 
@@ -61,10 +80,22 @@ class VelocityInlet:
         data['frame_of_reference'] = self._FRAME_OF_REFERENCE.get(self.frame_of_reference, 'unknown')
         data['ke_spec'] = self._KE_SPEC.get(self.ke_spec, 'unknown')
 
+        match data['ke_spec']:
+            case 'Intensity and Length Scale':
+                data.pop('turb_viscosity_ratio', None)
+                data.pop('turb_hydraulic_diam', None)
+            case 'Intensity and Viscosity Ratio':
+                data.pop('turb_length_scale', None)
+                data.pop('turb_hydraulic_diam', None)
+            case 'Intensity and Hydraulic Diameter':
+                data.pop('turb_length_scale', None)
+                data.pop('turb_viscosity_ratio', None)
+
         return data
 
 
 @dataclass
+@BoundaryFactory.register('mass-flow-inlet')
 class MassFlowInlet:
     name: str
     id_: str
@@ -76,6 +107,7 @@ class MassFlowInlet:
 
 
 @dataclass
+@BoundaryFactory.register('mass-flow-outlet')
 class MassFlowOutlet:
     name: str
     id_: str
@@ -87,23 +119,72 @@ class MassFlowOutlet:
 
 
 @dataclass
+@BoundaryFactory.register('pressure-inlet')
+class PressureInlet:
+    name: str
+    id_: str
+    frame_of_reference: str = ''
+    p0: str = ''
+    p: str = ''
+    t0: str = ''
+    ke_spec: str = ''
+    prevent_reverse_flow: str = ''
+    turb_intensity: str = ''
+    turb_length_scale: str = ''
+    turb_hydraulic_diam: str = ''
+    turb_viscosity_ratio: str = ''
+
+    _FRAME_OF_REFERENCE = {
+        '0': 'Absolute',
+        '1': 'Ralative to Adjacent Cell Zone',
+    }
+
+    _KE_SPEC = {
+        '1': 'Intensity and Length Scale',
+        '2': 'Intensity and Viscosity Ratio',
+        '3': 'Intensity and Hydraulic Diameter',
+    }
+
+    def to_dict(self) -> dict[str, str]:
+        data = self.__dict__.copy()
+
+        data['frame_of_reference'] = self._FRAME_OF_REFERENCE.get(self.frame_of_reference, 'unknown')
+        data['ke_spec'] = self._KE_SPEC.get(self.ke_spec, 'unknown')
+
+        match data['ke_spec']:
+            case 'Intensity and Length Scale':
+                data.pop('turb_viscosity_ratio', None)
+                data.pop('turb_hydraulic_diam', None)
+            case 'Intensity and Viscosity Ratio':
+                data.pop('turb_length_scale', None)
+                data.pop('turb_hydraulic_diam', None)
+            case 'Intensity and Hydraulic Diameter':
+                data.pop('turb_length_scale', None)
+                data.pop('turb_viscosity_ratio', None)
+
+        return data
+
+
+@dataclass
+@BoundaryFactory.register('pressure-outlet')
 class PressureOutlet:
     name: str
     id_: str
     p: str = ''
-    t: str = ''
+    t0: str = ''
     ke_spec: str = ''
     prevent_reverse_flow: str = ''
     radial: str = ''
     avg_press_spec: str = ''
     turb_intensity: str = ''
+    turb_length_scale: str = ''
     targeted_mf_boundary: str = ''
     turb_hydraulic_diam: str = ''
     turb_viscosity_ratio: str = ''
 
     _KE_SPEC = {
         '1': 'Intensity and Length Scale',
-        '2': 'Intensith and Viscosity Ratio',
+        '2': 'Intensity and Viscosity Ratio',
         '3': 'Intensity and Hydraulic Diameter',
     }
 
@@ -111,13 +192,26 @@ class PressureOutlet:
         data = self.__dict__.copy()
 
         if self.prevent_reverse_flow == '#t':
-            for key in ['t', 'ke_spec', 'turb_intensity', 'targeted_mf_boundary', 'turb_hydraulic_diam', 'turb_viscosity_ratio']:
+            for key in ['t', 'ke_spec', 'turb_intensity', 'turb_length_scale', 'targeted_mf_boundary', 'turb_hydraulic_diam', 'turb_viscosity_ratio']:
                 data.pop(key, None)
+        else:
+            data['ke_spec'] = self._KE_SPEC.get(self.ke_spec, 'unknown')
+            match data['ke_spec']:
+                case 'Intensity and Length Scale':
+                    data.pop('turb_viscosity_ratio', None)
+                    data.pop('turb_hydraulic_diam', None)
+                case 'Intensity and Viscosity Ratio':
+                    data.pop('turb_length_scale', None)
+                    data.pop('turb_hydraulic_diam', None)
+                case 'Intensity and Hydraulic Diameter':
+                    data.pop('turb_length_scale', None)
+                    data.pop('turb_viscosity_ratio', None)
 
         return data
 
 
 @dataclass
+@BoundaryFactory.register('wall')
 class Wall:
     name: str
     id_: str
@@ -149,7 +243,7 @@ class Wall:
     _ROUGH_BC = {}
 
     _THERMAL_BC_WHITELIST = {
-        '1': {'q'},  # Heat Flux
+        '1': {'q_dot'},  # Heat Flux
         '2': {'t'},  # Temperature
         '3': set(),  # Coupled
     }
@@ -158,7 +252,7 @@ class Wall:
         data = self.__dict__.copy()
 
         allowed_attrs = self._THERMAL_BC_WHITELIST.get(self.thermal_bc, set())
-        all_thermal_attrs = {'t', 'q', 'h'}
+        all_thermal_attrs = {'t', 'q_dot', 'h', 'q'}
         attrs_to_remove = all_thermal_attrs - allowed_attrs
         for attr in attrs_to_remove:
             data.pop(attr, None)
@@ -169,6 +263,7 @@ class Wall:
 
 
 @dataclass
+@BoundaryFactory.register('porous-jump')
 class PorousJump:
     name: str
     id_: str
@@ -178,24 +273,28 @@ class PorousJump:
 
 
 @dataclass
+@BoundaryFactory.register('fan')
 class Fan:
     name: str
     id_: str
 
 
 @dataclass
+@BoundaryFactory.register('radiator')
 class Radiator:
     name: str
     id_: str
 
 
 @dataclass
+@BoundaryFactory.register('interior')
 class Interior:
     name: str
     id_: str
 
 
 @dataclass
+@BoundaryFactory.register('symmetry')
 class Symmetry:
     name: str
     id_: str
@@ -205,31 +304,3 @@ class Symmetry:
 class NotImplementedBoundary:
     name: str
     id_: str
-
-
-class BoundaryFactory:
-    @staticmethod
-    def create(name: str, id_: str, type_: str):
-        match type_:
-            case 'fluid':
-                return Fluid(name, id_)
-            case 'solid':
-                return Solid(name, id_)
-            case 'velocity-inlet':
-                return VelocityInlet(name, id_)
-            case 'mass-flow-inlet':
-                return MassFlowInlet(name, id_)
-            case 'mass-flow-outlet':
-                return MassFlowOutlet(name, id_)
-            case 'pressure-outlet':
-                return PressureOutlet(name, id_)
-            case 'wall':
-                return Wall(name, id_)
-            case 'interior':
-                return Interior(name, id_)
-            case 'symmetry':
-                return Symmetry(name, id_)
-            case 'porous-jump':
-                return PorousJump(name, id_)
-            case _:
-                return NotImplementedBoundary(name, id_)
