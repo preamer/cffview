c1955c754f3ec7551e95969f8ca02e4380768b43
preamer <hjw2129@qq.com>
2026-07-27 20:28:48 +0800

    Add keyboard shortcuts hint before plotting mesh

--- src/cffview/main.py
+++ src/cffview/main.py
@@ -516,6 +516,9 @@ def show_mesh(file_path: str) -> None:
         Path to the .h5 file
     """
     import pyvista as pv
+    from pprint import pprint
+    from .utils import KEYBOARD_SHORTCUTS
+
     if file_path.endswith('cas.h5'):
         mesh = pv.read(file_path)
     elif file_path.endswith('msh.h5'):
@@ -618,6 +621,7 @@ def show_mesh(file_path: str) -> None:
     pl.add_text("Clip Plane", position=(60, 20), font_size=10)
 
     pl.add_axes(viewport=(0.8, 0.0, 1.0, 0.2))
+    pprint(KEYBOARD_SHORTCUTS, sort_dicts=False)
     pl.show()
 
 
--- src/cffview/utils.py
+++ src/cffview/utils.py
@@ -1,3 +1,5 @@
+import sys
+
 # From context.scm
 FLUENT_ENUM = {
     "0": "First Order Upwind",
@@ -62,6 +64,23 @@ CELL_TYPES = {
     5: "Pyramid",
     6: "Wedge",
     7: "Polyhedron",
+}
+
+KEYBOARD_SHORTCUTS = {
+    'q': 'Close the rendering window',
+    'f': 'Focus and zoom in on a point',
+    'v': 'Isometric camera view',
+    'w': 'Switch all datasets to a wireframe representation',
+    'r': 'Reset the camera to view all datasets',
+    's': 'Switch all datasets to a surface representation',
+    'shift+click or middle-click' if not sys.platform.startswith('darwin') else 'shift+click': 'Pan the rendering scene',
+    'left-click' if not sys.platform.startswith('darwin') else 'cmd+click': 'Rotate the rendering scene in 3D',
+    'ctrl+click': 'Rotate the rendering scene in 2D(view-plane)',
+    'mouse-wheel or right-click' if not sys.platform.startswith('darwin') else 'ctl+click': 'Continuously zoom the rendering scene',
+    'shift+s': 'Save a screenshot(only on BackgroundPlotter)',
+    'shift+c': 'Enable interactive cell selection/picking',
+    'up/down': 'Zoom in and out',
+    '+ /-': 'Increase/decrease the point size and line widths',
 }
 
 
